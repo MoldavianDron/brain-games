c868f749d87283fad56885b5adceff941b06c905
Andrei Zaporojan <moldaviandron@gmail.com>
2022-01-18 22:23:45 +0200

    Feat: add brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import brainGamesLogic from '../src/brain-games-logic.js';
+import randomInt from '../src/random-int.js';
+
+const isPrime = (num) => {
+    const isPrimeIter = (iter) => {
+        if (iter > Math.floor(num / 2)) return true;
+        if (num % iter === 0) return false;
+        return isPrimeIter(iter + 1);
+    };
+    if (num <= 3) return true;
+    if (num % 2 === 0) return false;
+    return isPrimeIter(3);
+};
+
+const explainBrainPrimeionRule = () => 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateBrainPrimeQuestionAnswerPair = () => {
+    const num = randomInt(1, 51);
+    if (isPrime(num)) {
+        return [num.toString(), 'yes'];
+    } return [num.toString(), 'no'];
+};
+
+brainGamesLogic(
+    explainBrainPrimeionRule,
+    generateBrainPrimeQuestionAnswerPair,
+);
